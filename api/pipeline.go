9a555372827b71279dd52c1908bb126b5c7bc60f
Brian Williams <bwilliams@gitlab.com>
2022-11-15 15:08:04 -0600

    ci: Add `go fmt` check to the CI pipeline
    Ensure that all files are formatted with `go fmt`.

--- api/pipeline.go
+++ api/pipeline.go
@@ -179,9 +179,9 @@ var GetPipelineVariables = func(client *gitlab.Client, pid int, l *gitlab.Reques
 	}
 
 	pipe, _, err := client.Pipelines.GetPipeline(repo, pid)
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 	projectID := pipe.ProjectID
 
 	pipelineVars, _, err := client.Pipelines.GetPipelineVariables(projectID, pid)
