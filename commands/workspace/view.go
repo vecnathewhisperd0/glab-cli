7361e6a3cda379850b5ddb4af62b61c77b7b28ac
Hunar Khanna <hunar.khanna@shopee.com>
2023-02-13 14:47:44 +0800

    feat(view-workspace): support json output

--- commands/workspace/view.go
+++ commands/workspace/view.go
@@ -1,6 +1,7 @@
 package workspace
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -24,6 +25,7 @@ type ViewOptions struct {
 	Group string
 	ID    string
 	Watch bool
+	Json  bool
 
 	IO            *iostreams.IOStreams
 	GraphQLClient *graphql.Client
@@ -64,6 +66,12 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 			}
 			opts.Watch = watchCount > 0
 
+			jsonCount, err := cmd.Flags().GetCount("json")
+			if err != nil {
+				return err
+			}
+			opts.Json = jsonCount > 0
+
 			return viewRun(opts)
 		},
 	}
@@ -71,6 +79,7 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 	cmdutils.EnableRepoOverride(workspaceViewCmd, f)
 	workspaceViewCmd.PersistentFlags().StringP("group", "g", "", "Select a group/subgroup. This option is ignored if a repo argument is set.")
 	workspaceViewCmd.Flags().CountP("watch", "w", "Watch for updates")
+	workspaceViewCmd.Flags().Count("json", "Render data as json")
 
 	return workspaceViewCmd
 }
@@ -86,8 +95,23 @@ func viewRun(opts *ViewOptions) error {
 		}
 
 		var output string
-		output += RenderWorkspace(opts.IO, workspace)
-		output += fmt.Sprintf("\nLatest data as of %s\n", time.Now().Format(time.Stamp))
+
+		// todo: watch mode doesn't particularly well with json
+		// 	leads to errors in parsing and therefore rendering (maybe due to \n embedded in the devfiles being serialized)
+		//
+		//  need to discuss this
+		if opts.Json {
+			var raw []byte
+			raw, err = json.Marshal(workspace)
+			if err != nil {
+				return "", err
+			}
+
+			output += string(raw)
+		} else {
+			output += RenderWorkspace(opts.IO, workspace)
+			output += fmt.Sprintf("\nLatest data as of %s\n", time.Now().Format(time.Stamp))
+		}
 
 		return output, nil
 	}
