0243c8cb853cb48349c680fd09a0aca946c0245c
unset <fveillette@gitlab.com>
2024-04-26 09:27:52 -0400

    Add GitLab copyright in license

--- commands/auth/generate/dpop/dpop.go
+++ commands/auth/generate/dpop/dpop.go
@@ -13,7 +13,7 @@ import (
 MIT License
 
 Copyright (c) 2023 Software go-dpop
-
+Portions Copyright 2024 Gitlab B.V
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
