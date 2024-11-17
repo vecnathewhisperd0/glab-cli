2a78f85af622f693bb86c00d14b129eb32b1d533
nuzzzen <freecandy420@protonmail.com>
2024-11-17 01:15:34 +0100

    feat(members): Add tests for members command
    - Add members_test.go to handle tests for "members" command
    
    Delivers #7299

---
+++ commands/project/members/members_test.go
@@ -0,0 +1,42 @@
+package members
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/gitlab-org/cli/commands/cmdutils"
+	"testing"
+)
+
+var tests = []struct {
+	expectedUse   string
+	expectedShort string
+	subcommands   []string
+}{
+	{
+		expectedUse:   "members [command] [flags]",
+		expectedShort: "Manage project members.",
+		subcommands:   []string{"add", "remove"},
+	},
+}
+
+func TestNewCmdMembers(t *testing.T) {
+
+	cmd := NewCmdMembers(&cmdutils.Factory{})
+	assert.Equal(t, tests[0].expectedUse, cmd.Use)
+	assert.Equal(t, tests[0].expectedShort, cmd.Short)
+
+	b := new(bytes.Buffer)
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"--help"})
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+}
+
+func TestNewCmdMembersSubcommands(t *testing.T) {
+	cmd := NewCmdMembers(&cmdutils.Factory{})
+	subcommands := cmd.Commands()
+	assert.NotNilf(t, subcommands, "expected subcommands not to be nil")
+	assert.True(t, cmd.HasAvailableSubCommands())
+	assert.Len(t, subcommands, len(tests[0].subcommands))
+}
