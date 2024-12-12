2fee7888735c62fbeb92b8d6ad4a96ca9cfd7eb1
Anders Parslov <anders.parslov@tryg.dk>
2024-12-12 18:07:09 +0100

    manually revert

--- pkg/git/stack_struct.go
+++ pkg/git/stack_struct.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/fs"
 	"iter"
-	"maps"
 	"os"
 	"path/filepath"
 	"strings"
@@ -68,14 +67,6 @@ func (s *Stack) RemoveRef(ref StackRef) error {
 	return nil
 }
 
-func (s *Stack) Branches() (branches []string) {
-	for _, v := range maps.All(s.Refs) {
-		branches = append(branches, v.Branch)
-	}
-
-	return branches
-}
-
 func (s *Stack) RemoveBranch(ref StackRef) error {
 	var branch string
 	var err error
@@ -133,6 +124,16 @@ func (s *Stack) adjustAdjacentRefs(ref StackRef) error {
 	}
 
 	return nil
+}
+
+func (s *Stack) IndexAt(ref StackRef) int {
+	for i, r := range s.Iter2() {
+		if r == ref {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func (s *Stack) Last() StackRef {
@@ -290,7 +291,7 @@ func validateStackRefs(s Stack) error {
 	return nil
 }
 
-func CurrentStackRefFromBranch(title string) (StackRef, error) {
+func CurrentStackRefFromCurrentBranch(title string) (StackRef, error) {
 	stack, err := GatherStackRefs(title)
 	if err != nil {
 		return StackRef{}, err
@@ -301,13 +302,17 @@ func CurrentStackRefFromBranch(title string) (StackRef, error) {
 		return StackRef{}, err
 	}
 
-	for _, ref := range stack.Refs {
+	return stack.RefFromBranch(branch)
+}
+
+func (s Stack) RefFromBranch(branch string) (StackRef, error) {
+	for ref := range s.Iter() {
 		if ref.Branch == branch {
 			return ref, nil
 		}
 	}
 
-	return StackRef{}, nil
+	return StackRef{}, errors.New("Could not find stack ref for branch: " + branch)
 }
 
 // Empty returns true if the stack ref does not have an associated SHA (commit).
