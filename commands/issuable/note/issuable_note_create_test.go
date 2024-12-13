2fdd0a54f6d2682d0d0699433a97793a9ee40e6b
Gary Holtz <gholtz@gitlab.com>
2024-12-12 19:09:03 -0600

    chore: Fixing a test that keeps failing

--- commands/issuable/note/issuable_note_create_test.go
+++ commands/issuable/note/issuable_note_create_test.go
@@ -3,6 +3,7 @@ package note
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"reflect"
 	"testing"
 
@@ -207,7 +208,7 @@ func Test_IssuableNoteCreate_prompt(t *testing.T) {
 			}
 			assert.Equal(t, "", output.Stderr())
 			assert.Equal(t, "https://gitlab.com/OWNER/REPO/issues/1#note_301\n", output.String())
-			assert.Equal(t, "vi", actualEditor)
+			assert.Equal(t, os.Getenv("EDITOR"), actualEditor)
 		})
 
 		tests := []struct {
